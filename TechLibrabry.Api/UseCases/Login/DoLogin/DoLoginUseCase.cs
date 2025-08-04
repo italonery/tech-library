f3af49a6f646eefb52b9665c7bddcd24d9996cf0
italonery <babaduquedev@gmail.com>
2025-08-04 00:04:23 -0300

    feat: create login endpoint

---
+++ TechLibrabry.Api/UseCases/Login/DoLogin/DoLoginUseCase.cs
@@ -0,0 +1,40 @@
+﻿using TechLibrary.Api.Infraestructure.DataAccess;
+using TechLibrary.Api.Infraestructure.Security.Cryptography;
+using TechLibrary.Api.Infraestructure.Security.Tokens.Access;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCases.Login.DoLogin
+{
+    public class DoLoginUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestLoginJson request)
+        {
+            var dbContext = new TechLibraryDbContext();
+
+            // First (eu tenho certeza que o e-mail existe, se não encontrar gera uma runtime exception)
+            // FirstOrDefault (caso não encontre vai me devolver nulo)
+            var user = dbContext.Users.FirstOrDefault(user => user.Email.Equals(request.Email));
+            if (user is null)
+            {
+                throw new InvalidLoginException();
+            }
+
+            var cryptography = new BCryptAlgorithm();
+            var passwordIsValid = cryptography.Verify(request.Password, user);
+            if (passwordIsValid == false)
+            {
+                throw new InvalidLoginException();
+            }
+
+            var tokenGenerator = new JwtTokenGenerator();
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = user.Name,
+                AccessToken = tokenGenerator.Generate(user)
+            };
+        }
+    }
+}
---
+++ TechLibrary.Exception/InvalidLoginException.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace TechLibrary.Exception
+{
+    public class InvalidLoginException : TechLibraryException
+    {
+        public override List<string> GetErrorMessages() => /*Simplificado*/ ["E-mail e/ou senha inválidos"]; // new List<string> { "E-mail e/ou senha inválidos" };
+
+        public override HttpStatusCode GetStatusCode() => HttpStatusCode.Unauthorized;
+    }
+}
